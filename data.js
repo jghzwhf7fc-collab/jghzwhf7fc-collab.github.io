80716bd6f37477a8926d725c4d6daa8fd4f6ca22
jghzwhf7fc-collab <jghzwhf7fc@privaterelay.appleid.com>
2026-06-28 16:14:51 +0900

    data.js を更新

--- data.js
+++ data.js
@@ -1,79 +1,60 @@
 const record = {
-  lastUpdated: "2026/06/28",
+  title: "乱獲記録",
+
   total: {
     day: 1615,
     night: 1652
   },
+
   stages: [
     {
+      key: "aramaki",
       short: "砦",
       name: "アラマキ砦",
-      badge: "images/aramaki-badge.png",
-      stageImage: "images/aramaki-stage.jpg",
-      dayScore: 219,
-      nightScore: 241,
-      dayResult: "images/aramaki-day.jpg",
-      nightResult: "images/aramaki-night.jpg"
+      day: 219,
+      night: 241
     },
     {
+      key: "dam",
       short: "ダ",
       name: "シェケナダム",
-      badge: "images/dam-badge.png",
-      stageImage: "images/dam-stage.jpg",
-      dayScore: 226,
-      nightScore: 233,
-      dayResult: "images/dam-day.jpg",
-      nightResult: "images/dam-night.jpg"
+      day: 226,
+      night: 233
     },
     {
+      key: "muni",
       short: "ム",
       name: "ムニ・エール海洋発電所",
-      badge: "images/muni-badge.png",
-      stageImage: "images/muni-stage.jpg",
-      dayScore: 238,
-      nightScore: 237,
-      dayResult: "images/muni-day.jpg",
-      nightResult: "images/muni-night.jpg"
+      day: 238,
+      night: 237
     },
     {
+      key: "donbraco",
       short: "船",
       name: "難破船ドン・ブラコ",
-      badge: "images/donbraco-badge.png",
-      stageImage: "images/donbraco-stage.jpg",
-      dayScore: 210,
-      nightScore: 227,
-      dayResult: "images/donbraco-day.jpg",
-      nightResult: "images/donbraco-night.jpg"
+      day: 210,
+      night: 227
     },
     {
+      key: "sujiko",
       short: "筋",
       name: "すじこジャンクション跡",
-      badge: "images/sujiko-badge.png",
-      stageImage: "images/sujiko-stage.jpg",
-      dayScore: 252,
-      nightScore: 248,
-      dayResult: "images/sujiko-day.jpg",
-      nightResult: "images/sujiko-night.jpg"
+      day: 252,
+      night: 248
     },
     {
+      key: "tokishirazu",
       short: "時",
       name: "トキシラズいぶし工房",
-      badge: "images/tokishirazu-badge.png",
-      stageImage: "images/tokishirazu-stage.jpg",
-      dayScore: 232,
-      nightScore: 229,
-      dayResult: "images/tokishirazu-day.jpg",
-      nightResult: "images/tokishirazu-night.jpg"
+      day: 232,
+      night: 229
     },
     {
+      key: "donpiko",
       short: "ど",
       name: "どんぴこ闘技場",
-      badge: "images/donpiko-badge.png",
-      stageImage: "images/donpiko-stage.jpg",
-      dayScore: 238,
-      nightScore: 237,
-      dayResult: "images/donpiko-day.jpg",
-      nightResult: "images/donpiko-night.jpg"
+      day: 238,
+      night: 237
     }
   ]
 };
