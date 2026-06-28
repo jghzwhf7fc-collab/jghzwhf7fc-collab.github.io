d21075602bcae618ec7fc862aca1398a56b0ae3f
jghzwhf7fc-collab <jghzwhf7fc@privaterelay.appleid.com>
2026-06-28 15:40:39 +0900

    data.js を作成

---
+++ data.js
@@ -0,0 +1,64 @@
+const record = {
+  total: {
+    day: 1615,
+    night: 1652
+  },
+  stages: [
+    {
+      short: "砦",
+      name: "アラマキ砦",
+      day: 219,
+      night: 241,
+      image: "images/aramaki.jpg",
+      memo: "最高記録スクショ"
+    },
+    {
+      short: "ダ",
+      name: "シェケナダム",
+      day: 226,
+      night: 233,
+      image: "images/dam.jpg",
+      memo: "最高記録スクショ"
+    },
+    {
+      short: "ム",
+      name: "ムニ・エール海洋発電所",
+      day: 238,
+      night: 237,
+      image: "images/muni.jpg",
+      memo: "最高記録スクショ"
+    },
+    {
+      short: "船",
+      name: "難破船ドン・ブラコ",
+      day: 210,
+      night: 227,
+      image: "images/donbraco.jpg",
+      memo: "最高記録スクショ"
+    },
+    {
+      short: "筋",
+      name: "すじこジャンクション跡",
+      day: 252,
+      night: 248,
+      image: "images/sujiko.jpg",
+      memo: "最高記録スクショ"
+    },
+    {
+      short: "時",
+      name: "トキシラズいぶし工房",
+      day: 232,
+      night: 229,
+      image: "images/tokishirazu.jpg",
+      memo: "最高記録スクショ"
+    },
+    {
+      short: "ど",
+      name: "どんぴこ闘技場",
+      day: 238,
+      night: 237,
+      image: "images/donpiko.jpg",
+      memo: "最高記録スクショ"
+    }
+  ]
+};
