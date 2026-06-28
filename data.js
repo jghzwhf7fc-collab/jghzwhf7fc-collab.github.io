6777aeea69accf0a8e356ffb3bec61d58d1bc9ec
jghzwhf7fc-collab <jghzwhf7fc@privaterelay.appleid.com>
2026-06-28 15:59:07 +0900

    data.js を更新

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 const record = {
+  lastUpdated: "2026/06/28",
   total: {
     day: 1615,
     night: 1652
@@ -7,58 +8,72 @@ const record = {
     {
       short: "砦",
       name: "アラマキ砦",
-      day: 219,
-      night: 241,
-      image: "images/aramaki.jpg",
-      memo: "最高記録スクショ"
+      badge: "images/aramaki-badge.png",
+      stageImage: "images/aramaki-stage.jpg",
+      dayScore: 219,
+      nightScore: 241,
+      dayResult: "images/aramaki-day.jpg",
+      nightResult: "images/aramaki-night.jpg"
     },
     {
       short: "ダ",
       name: "シェケナダム",
-      day: 226,
-      night: 233,
-      image: "images/dam.jpg",
-      memo: "最高記録スクショ"
+      badge: "images/dam-badge.png",
+      stageImage: "images/dam-stage.jpg",
+      dayScore: 226,
+      nightScore: 233,
+      dayResult: "images/dam-day.jpg",
+      nightResult: "images/dam-night.jpg"
     },
     {
       short: "ム",
       name: "ムニ・エール海洋発電所",
-      day: 238,
-      night: 237,
-      image: "images/muni.jpg",
-      memo: "最高記録スクショ"
+      badge: "images/muni-badge.png",
+      stageImage: "images/muni-stage.jpg",
+      dayScore: 238,
+      nightScore: 237,
+      dayResult: "images/muni-day.jpg",
+      nightResult: "images/muni-night.jpg"
     },
     {
       short: "船",
       name: "難破船ドン・ブラコ",
-      day: 210,
-      night: 227,
-      image: "images/donbraco.jpg",
-      memo: "最高記録スクショ"
+      badge: "images/donbraco-badge.png",
+      stageImage: "images/donbraco-stage.jpg",
+      dayScore: 210,
+      nightScore: 227,
+      dayResult: "images/donbraco-day.jpg",
+      nightResult: "images/donbraco-night.jpg"
     },
     {
       short: "筋",
       name: "すじこジャンクション跡",
-      day: 252,
-      night: 248,
-      image: "images/sujiko.jpg",
-      memo: "最高記録スクショ"
+      badge: "images/sujiko-badge.png",
+      stageImage: "images/sujiko-stage.jpg",
+      dayScore: 252,
+      nightScore: 248,
+      dayResult: "images/sujiko-day.jpg",
+      nightResult: "images/sujiko-night.jpg"
     },
     {
       short: "時",
       name: "トキシラズいぶし工房",
-      day: 232,
-      night: 229,
-      image: "images/tokishirazu.jpg",
-      memo: "最高記録スクショ"
+      badge: "images/tokishirazu-badge.png",
+      stageImage: "images/tokishirazu-stage.jpg",
+      dayScore: 232,
+      nightScore: 229,
+      dayResult: "images/tokishirazu-day.jpg",
+      nightResult: "images/tokishirazu-night.jpg"
     },
     {
       short: "ど",
       name: "どんぴこ闘技場",
-      day: 238,
-      night: 237,
-      image: "images/donpiko.jpg",
-      memo: "最高記録スクショ"
+      badge: "images/donpiko-badge.png",
+      stageImage: "images/donpiko-stage.jpg",
+      dayScore: 238,
+      nightScore: 237,
+      dayResult: "images/donpiko-day.jpg",
+      nightResult: "images/donpiko-night.jpg"
     }
   ]
 };
